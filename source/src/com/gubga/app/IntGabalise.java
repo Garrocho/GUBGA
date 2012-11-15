2b177a2f8a94b6ce99cab7f83462ba340c4e97d2
CharlesGarrocho <ctgarrocho@gmail.com>
2012-11-15 17:45:40 -0200

    Alterado: Modificado os nomes dos pacotes.

---
+++ source/src/com/gubga/app/IntGabalise.java
@@ -0,0 +1,17 @@
+package com.gubga.app;
+
+import java.io.IOException;
+
+import jpcap.JpcapCaptor;
+
+import com.gubga.pacote.RecebimentoPacote;
+
+
+public class IntGabalise {
+
+	public static void main(String args[]) throws IOException {
+		jpcap.NetworkInterface[] devices = JpcapCaptor.getDeviceList();
+		JpcapCaptor jpcap = JpcapCaptor.openDevice(devices[0], 2000, true, 20);
+		jpcap.loopPacket(-1, new RecebimentoPacote());
+	}
+}
