0b6d6e48d65391fb2119ee16dc057c1c599810b5
Charles Tim Batista Garrocho <ctgarrocho@gmail.com>
2012-11-17 14:09:55 -0200

    Adicionado: Carregamento das Contas de usuários da pasta room/user

--- source/src/com/gubga/gui/carregamento/eventos/TratadorEventosJanelaCarregamento.java
+++ source/src/com/gubga/gui/carregamento/eventos/TratadorEventosJanelaCarregamento.java
@@ -6,13 +6,14 @@ import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
-import java.io.IOException;
-import java.net.DatagramSocket;
-import java.net.SocketException;
+import java.io.File;
 
+import javax.swing.JOptionPane;
 import javax.swing.table.DefaultTableModel;
 
+import com.gubga.gui.Janela;
 import com.gubga.gui.carregamento.JanelaCarregamento;
+import com.sun.org.apache.bcel.internal.classfile.Field;
 
 public class TratadorEventosJanelaCarregamento extends KeyAdapter implements ActionListener, MouseListener {
 
@@ -26,6 +27,50 @@ public class TratadorEventosJanelaCarregamento extends KeyAdapter implements Act
 	}
 
 	public void actionPerformed(ActionEvent evento) {
+		
+		// Caso o evento tenha ocorrido no botao Diretorio.
+		if (evento.getSource() == janelaCarregamento.getBotaoDiretorio()) {
+			String diretorio = Janela.dialogoAbrirDiretorio(janelaCarregamento, "Selecione o Diretório da Pasta do Garena Plus.");
+			if (diretorio != null)
+				janelaCarregamento.getCampoTextoDiretorio().setText(diretorio);
+			else
+				janelaCarregamento.getCampoTextoDiretorio().setText("Diretório do Garena Plus.");
+		}
+		
+		// Caso o evento tenha ocorrido no botao Sair.
+		else if (evento.getSource() == janelaCarregamento.getBotaoSair()) {
+			System.exit(0);
+		}
+		
+		// Caso o evento tenha ocorrido no botao Limpar.
+		else if (evento.getSource() == janelaCarregamento.getBotaoLimpar()) {
+
+			modeloTabela = ((DefaultTableModel)(janelaCarregamento.getTabelaUsuarios().getModel()));
+			modeloTabela.setNumRows(0);
+		}
+		
+		// Caso o evento tenha ocorrido no botao Carregar.
+		else if (evento.getSource() == janelaCarregamento.getBotaoCarregar()) {
+			String diretorio = janelaCarregamento.getCampoTextoDiretorio().getText();
+			if (diretorio.equalsIgnoreCase("Diretório do Garena Plus."))
+				JOptionPane.showMessageDialog(janelaCarregamento, "O Diretório do Garena Não Foi Selecionado.", "Erro ao Abrir Diretório", JOptionPane.ERROR_MESSAGE);
+			else {
+				File d2 = new File(diretorio + "/room/user/");
+				if (!d2.exists())
+					JOptionPane.showMessageDialog(janelaCarregamento, "O Diretório Selecionado Não é Um Diretório do Garena Plus.", "Erro de Diretório Inválido", JOptionPane.ERROR_MESSAGE);
+				else {
+					String userIds[] = d2.list();
+					System.out.println(d2.getAbsolutePath());
+					modeloTabela = ((DefaultTableModel)(janelaCarregamento.getTabelaUsuarios().getModel()));
+					modeloTabela.setNumRows(0);
+					Object[] linha = new Object[1];
+					for (String user : userIds) {
+						linha[0] = user;
+						modeloTabela.addRow(linha);
+					}
+				}
+			}
+		}
 	}
 
 	public void keyReleased(KeyEvent event) {
