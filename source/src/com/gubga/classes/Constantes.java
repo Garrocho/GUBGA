117d6fc94ac0d02a0942b4a3a0e2c1bfba2690d0
Charles Tim Batista Garrocho <ctgarrocho@gmail.com>
2012-11-28 23:50:15 -0200

    Removido: Arquivos Incorretos.

--- source/src/com/gubga/classes/Constantes.java
+++
@@ -1,8 +0,0 @@
-package com.gubga.classes;
-
-public class Constantes {
-	
-	public static int ROOM_MENSAGEM_SALA = 620756992;
-	public static int ROOM_ENTROU_SALA = 570425344;
-	public static int ROOM_SAIU_SALA = 587202560;
-}
